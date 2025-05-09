f3e1a9d1cb8ee9d15aa7b886ed5f6d795426ee57
Jake Coffman <jake@jakecoffman.com>
2025-05-09 10:14:49 -0500

    add indirect dependency

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/dsp-testing/jake-golib
 
 go 1.22.0
+
+require golang.org/x/text v0.21.0
--- lib.go
+++ lib.go
@@ -1,5 +1,9 @@
 package jake_golib
 
+import (
+	_ "golang.org/x/text"
+)
+
 func Add(a, b int) int {
 	return a + b
 }
